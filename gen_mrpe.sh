557afb3ed95588b41053793db781be8a45b08d8e
Mehdi Abaakouk <theli48@gmail.com>
2011-10-31 11:32:29 +0100

    Add prefix for ipv6

--- gen_mrpe.sh
+++ gen_mrpe.sh
@@ -5,5 +5,5 @@ for i in $(echo show protocols | birdc  | awk '/Established/{if ($2 == "BGP") pr
 done
 
 for i in $(echo show protocols | birdc6  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
-    echo "BGP_$i /etc/check_mk/nagios-bird/check_bird_proto6.sh $i"
+    echo "BGP6_$i /etc/check_mk/nagios-bird/check_bird_proto6.sh $i"
 done
