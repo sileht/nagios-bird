e5ca28f270cc7116f5c342143f3c28b839b16c51
Mehdi Abaakouk <theli48@gmail.com>
2011-10-31 11:31:12 +0100

    Add mpre generator

---
+++ gen_mrpe.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in $(echo show protocols | birdc  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
+    echo "BGP_$i /etc/check_mk/nagios-bird/check_bird_proto.sh $i"
+done
+
+for i in $(echo show protocols | birdc6  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
+    echo "BGP_$i /etc/check_mk/nagios-bird/check_bird_proto6.sh $i"
+done
