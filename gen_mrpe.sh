fa63ffd06ab493b0dff5252229242800cc886d17
Mehdi Abaakouk <theli48@gmail.com>
2011-11-08 11:23:16 +0100

    Allow diff with current mpre.cfg file

--- gen_mrpe.sh
+++ gen_mrpe.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+(
 for i in $(echo show protocols | birdc  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
     echo "BGP_$i /etc/check_mk/nagios-bird/check_bird_proto.sh $i"
 done
@@ -7,3 +8,13 @@ done
 for i in $(echo show protocols | birdc6  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
     echo "BGP6_$i /etc/check_mk/nagios-bird/check_bird_proto6.sh $i"
 done
+) > /tmp/tmp_mrpe.cfg
+
+case "$1" in
+    -d|--diff)
+        diff -uNr /tmp/tmp_mrpe.cfg <(grep ^BGP /etc/check_mk/mrpe:cfg)
+        ;;
+    *)
+        cat /tmp/tmp_mrpe.cfg
+        ;;
+esac
