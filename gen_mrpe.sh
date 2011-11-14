685c7a28f83e4bbce3008929cbf52dcb2cd65664
Mehdi Abaakouk <theli48@gmail.com>
2011-11-14 20:22:36 +0100

    Add -t and -p

--- gen_mrpe.sh
+++ gen_mrpe.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-(
+{
+echo "BGP_SESSIONS /etc/check_mk/nagios-bird/gen_mrpe.sh -t" 
+
 for i in $(echo show protocols | birdc  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
     echo "BGP_$i /etc/check_mk/nagios-bird/check_bird_proto.sh $i"
 done
@@ -8,13 +10,31 @@ done
 for i in $(echo show protocols | birdc6  | awk '/Established/{if ($2 == "BGP") print $1}' | grep -v TETANEUTRAL); do
     echo "BGP6_$i /etc/check_mk/nagios-bird/check_bird_proto6.sh $i"
 done
-) > /tmp/tmp_mrpe.cfg
+} > /tmp/tmp_mrpe.cfg
 
 case "$1" in
     -d|--diff)
         diff -uNr <(grep ^BGP /etc/check_mk/mrpe.cfg) /tmp/tmp_mrpe.cfg 
+        exit 0
+        ;;
+    -t)
+        data=$($0 -d | awk '/^+BGP/{printf $3" "}' )
+        nb_bgp=$(grep -c 'check_bird_proto' /etc/check_mk/mrpe.cfg)
+        if [ "$data" ] ; then
+            echo "$nb_bgp actives BGP sessions, New BGP session discover: $data"
+            exit 2
+        else
+            echo "$nb_bgp actives BGP sessions"
+            exit 0
+        fi
+        ;;
+    -p)
+        cp -f /tmp/tmp_mrpe.cfg /etc/check_mk/mrpe.cfg
+        exit 0
         ;;
     *)
         cat /tmp/tmp_mrpe.cfg
+        exit 0
         ;;
 esac
+exit 3
